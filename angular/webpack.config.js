e73be31bdb11f8e34bc636b7e1c9fab610202f6d
Thorgal Hayne <thorgal.hayne@outlook.com>
2016-06-09 22:09:17 +0100

    React on done & run-watch webpack event. Output colored text.

--- angular/webpack.config.js
+++ angular/webpack.config.js
@@ -1,11 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin  = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const helpers = require('./config/helpers');
 
 module.exports = {
     entry: {
-        'app': './src/main.ts',
+        'app': './src/main.ts'
     },
 
     output: {
@@ -18,9 +18,21 @@ module.exports = {
             from: 'src/assets',
             to: 'assets'
         }]),
-        
+
         new HtmlWebpackPlugin({
             template: 'src/index.html'
-        })
-    ],
-}
+        }),
+
+        function ReactOnDonePlugin() {
+            this.plugin('done', function () {
+                console.log('Webpack: \u001b[32;1mDONE!\u001b[0m');
+            })
+        },
+        function ReactOnWatchRunPlugin() {
+            this.plugin('watch-run', function (watching, callback) {
+                console.log('\u001b[30;1m' + new Date() + '\u001b[0m');
+                callback();
+            });
+        }
+    ]
+};
