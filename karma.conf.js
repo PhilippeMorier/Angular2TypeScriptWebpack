7a39c19710bab5b288ec9898a7caeb05e2215810
Philippe Morier <thorgal.hayne@outlook.com>
2016-06-28 19:34:56 +0100

    Use mocha reporter for more detailed unit test log

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'coverage', 'karma-remap-istanbul'],
+        reporters: ['mocha', 'coverage', 'karma-remap-istanbul'],
 
         coverageReporter: {
             dir: '../../coverage',
