5e227d399efee30b12739361f11f6f87de1a8352
Paul Cadman <paul.cadman@zuhlke.com>
2016-08-12 15:07:54 +0100

    Update to angular 2.0.0-rc.5 - move routing to module decorator.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { routing, appRoutingProviders } from './app.routes';
 
 @NgModule({
     bootstrap:    [ AppComponent ],
-    declarations: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        ROUTER_DIRECTIVES
+    ],
     imports:      [
         BrowserModule,
         routing
