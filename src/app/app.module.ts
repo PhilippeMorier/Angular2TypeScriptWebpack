b0f53f74349cd2eab693774379ff1b5ea5c08522
Philippe Morier <thorgal.hayne@outlook.com>
2016-09-08 08:59:46 +0100

    fix(barrel): Use path for barrel instead of specific file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ConceptsAndAlgorithmsComponent } from './conceptsAndAlgorithms/concepts
 import { DataStructuresComponent } from './dataStructures/dataStructures.component.ts';
 import { ArraysAndStringsComponent } from './dataStructures/01ArraysAndStrings/arraysAndStrings.component.ts';
 import { LinkedListsComponent } from './dataStructures/02LinkedLists/linkedLists.component.ts';
-import { UniqueCharactersComponent } from './dataStructures/01ArraysAndStrings/01UniqueCharacters.ts';
+import { UniqueCharactersComponent } from './dataStructures/01ArraysAndStrings/01UniqueCharacters';
 import { ReverseStringComponent } from './dataStructures/01ArraysAndStrings/02ReverseString/reverseString.component.ts';
 import { IsPermutationComponent } from './dataStructures/01ArraysAndStrings/03IsPermutation/isPermutation.component.ts';
 import { ReplaceAllSpacesComponent } from './dataStructures/01ArraysAndStrings/04ReplaceAllSpaces/replaceAllSpaces.component.ts';
